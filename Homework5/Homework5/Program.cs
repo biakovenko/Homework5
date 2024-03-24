d64e48842e4533e2bd461451020be1f1e2ca6cef
biakovenko <146518765+biakovenko@users.noreply.github.com>
2024-03-24 23:09:31 +0300

    Add files via upload

---
+++ Homework5/Homework5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    internal class Homework5
+    {
+        static void Main()
+        {
+            Console.WriteLine("1 Задание:");
+            BinarFile();
+        }
+
+        static void BinarFile(int N = 20, double a = -50, double b = 50, string fileName = "task1.txt")
+        {
+            if (N <= 0 || a > b || string.IsNullOrWhiteSpace(fileName))
+            {
+                Console.WriteLine("Неправильные значения.");
+                return;
+            }
+        }
+    }
+}
